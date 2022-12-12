c1fe4c9f398a4bc13add7ab4a51f565c3b19f5fb
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-12-12 12:12:47 -0600

    change library properties and remote zip

--- build.sh
+++ build.sh
@@ -11,14 +11,15 @@ valgrind -v --log-file="valgrind_results.txt" .pio/build/native/program
 #for f in *.out; do
 #    valgrind -v --log-file="${f%.*}"_memory.txt ./$f
 #done
-mkdir blinky_utils-master
+mkdir blinky_utils
 cd src
-for f in *h; do 
-     cp "$f" "../blinky_utils/${f%.c}"
-     echo "$f"
-done
+#for f in *h; do 
+#     cp "$f" "../blinky_utils/${f%.c}"
+#     echo "$f"
+#done
 #cp *.h ../blinky_utils/
 cd ..
 #rm blinky_utils/main.cpp
-cp src/library_properties/* blinky_utils-master/
-zip -r blinky_utils-master.zip blinky_utils-master/
+cp src/* blinky_utils/
+#cp src/library_properties/* blinky_utils/
+zip -r blinky_utils-master.zip blinky_utils/
