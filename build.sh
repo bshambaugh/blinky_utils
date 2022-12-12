2e3b7fd53cbae386d4bb0426a8608b68bcfff497
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-12-12 11:46:20 -0600

    change blinky utils to blinky utils master

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ valgrind -v --log-file="valgrind_results.txt" .pio/build/native/program
 #for f in *.out; do
 #    valgrind -v --log-file="${f%.*}"_memory.txt ./$f
 #done
-mkdir blinky_utils
+mkdir blinky_utils-master
 cd src
 for f in *h; do 
      cp "$f" "../blinky_utils/${f%.c}"
